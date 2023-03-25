27b8e986072570fc9eb5772279b89b461c2784e0
VINIITH SHAH <99725376+viniith21@users.noreply.github.com>
2023-03-25 17:01:59 +0530

    new change

---
+++ MindAr/MindAr/VIDEO/import.js
@@ -0,0 +1,24 @@
+import MindAR from "mind-ar-sdk";
+
+// Load the video file
+const videoFile = "mp4.mp4";
+const video = new MindAR.Video(videoFile);
+
+// Set up the AR marker
+const arMarkerFile = "download1.jpg";
+const arMarker = new MindAR.ARMarker(arMarkerFile);
+
+// Create a new AR project
+const project = new MindAR.ARProject();
+
+// Add the video and AR marker to the project
+project.addVideo(video);
+project.addMarker(arMarker);
+
+// Set up the trigger to play the video when the AR marker is scanned
+project.addTrigger(arMarker, () => {
+  video.play();
+});
+
+// Start the Mind AR app and scan the AR marker to play the video
+MindAR.startApp(project);
