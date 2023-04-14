a80b5203711d218164dbdc7cf3436e760b51fc00
VINIITH SHAH <99725376+viniith21@users.noreply.github.com>
2023-04-14 18:05:18 +0530

    change

--- MindAr/MindAr/VIDEO/import.js
+++ MindAr/MindAr/VIDEO/import.js
@@ -1,8 +1,4 @@
-import * as MindAR from "mind-ar";
-// create an AR scene
 const scene = new MindAR.Scene();
-
-// add a video element to the scene
 const video = document.createElement("video");
 video.src = "mp4.mp4";
 video.autoplay = true;
@@ -15,7 +11,6 @@ const videoPlane = new MindAR.Plane({
 });
 scene.add(videoPlane);
 
-// detect a trigger image and play the video when detected
 const tracker = new MindAR.ImageTracker();
 const triggerImage = new MindAR.Image("download1.jpg");
 tracker.addImage(triggerImage, () => {
